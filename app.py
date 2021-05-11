bc9e21846b61c9447f73463fbfe0c1399d6c3844
kdsumit04 <kdsumit04@gmail.com>
2021-05-11 18:18:25 +0530

    logging is added to the app

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from flask import Flask
+import logging
 import requests
+
 app = Flask(__name__)
+
+#below 3 lines are used to remove a unwanted info which
+#was also getting printed
+app.logger.disabled = True
+log = logging.getLogger('werkzeug')
+log.disabled = True
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s %(asctime)s - %(message)s",
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
+
 @app.route("/say/<name>")
 def greeting(name):
     
-    x = requests.get('http://greeting-service.test:500/greeting/'+name)
+    logging.info("Greet-Client is Started")
+    x = requests.get('http://greeting-int-service.test:500/greeting/'+name)
     if x:
         return x.text
     else :
@@ -18,10 +29,4 @@ def greeting(name):
 
 if __name__ == "__main__":
     app.run(host ='0.0.0.0', port=90)
-
-
-# In[ ]:
-
-
-
 
